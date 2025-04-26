9b4cc495e165e32da65c9ed801a97c83e069a0e0
Rushikesh Sawarkar <rushikeshsawarkar14@gmail.com>
2025-04-26 22:20:21 +0530

    Changes kiye hai

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const port = 3000
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  let siteName = "Addidas"
-  let searchText = "Search now"
-  res.render("index", {siteName, searchText})
+  res.render("index")
 })
+
+app.get('/home', (req, res) => {
+  res.render('home')
+});
+
+app.get('/about', (req, res) => res.render('about'));
+app.get('/services', (req, res) => res.render('services'));
+app.get('/projects', (req, res) => res.render('projects'));
+app.get('./views/contact', (req, res) => res.render('contact'));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
